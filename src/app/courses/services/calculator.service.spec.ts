1d661d21b139855c293fe8e6e78f7a7cedc861f8
Kiran Anand <liteninkiran@hotmail.com>
2023-08-25 16:04:26 +0100

    007 - Unit Testing with Jasmine - Introduction to Jasmine Spies

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -4,13 +4,15 @@ import { LoggerService } from "./logger.service";
 describe('CalculatorService', () => {
     it('should add two numbers', () => {
         // Arrange
-        const calculator = new CalculatorService(new LoggerService());
+        const logger = jasmine.createSpyObj('LoggerService', ['log']);
+        const calculator = new CalculatorService(logger);
 
         // Act
         const result = calculator.add(2, 3);
 
         // Assert... use toBe() when comparing values and toEqual() when comparing objects
         expect(result).toBe(5);
+        expect(logger.log).toHaveBeenCalledTimes(1);
     });
 
     it('should subtract two numbers', () => {
