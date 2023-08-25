1fa1656386eb62df46c34efd58fb0129ac2f8dee
Kiran Anand <liteninkiran@hotmail.com>
2023-08-25 17:10:35 +0100

    011 - Angular Unit Testing Best Practices and commonly used Test Utilities

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -8,7 +8,6 @@ describe('CalculatorService', () => {
     let loggerSpy: any;
 
     beforeEach(() => {
-        // Arrange
         loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
         TestBed.configureTestingModule({
             providers: [
@@ -20,19 +19,13 @@ describe('CalculatorService', () => {
     });
 
     it('should add two numbers', () => {
-        // Act
         const result = calculator.add(2, 3);
-
-        // Assert... use toBe() when comparing values and toEqual() when comparing objects
         expect(result).toBe(5);
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
 
     it('should subtract two numbers', () => {
-        // Act
         const result = calculator.subtract(2, 3);
-
-        // Assert... use toBe() when comparing values and toEqual() when comparing objects
         expect(result).toBe(-1, 'Unexpected subtraction result');
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
