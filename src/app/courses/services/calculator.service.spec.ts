a12d0a4bea060bf741a0d2a638843e3be9741ac7
Kiran Anand <liteninkiran@hotmail.com>
2023-08-25 13:40:25 +0100

    006 - Step-by-Step Implementation of our First Jasmine Specification

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,10 +1,26 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
     it('should add two numbers', () => {
-        pending();
+        // Arrange
+        const calculator = new CalculatorService(new LoggerService());
+
+        // Act
+        const result = calculator.add(2, 3);
+
+        // Assert... use toBe() when comparing values and toEqual() when comparing objects
+        expect(result).toBe(5);
     });
 
     it('should subtract two numbers', () => {
-        fail();
+        // Arrange
+        const calculator = new CalculatorService(new LoggerService());
+
+        // Act
+        const result = calculator.subtract(2, 3);
+
+        // Assert... use toBe() when comparing values and toEqual() when comparing objects
+        expect(result).toBe(-1, 'Unexpected subtraction result');
     });
 });
