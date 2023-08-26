271cdc174bc8ac368b7cd12ebd52858852e0a6bf
Kiran Anand <liteninkiran@hotmail.com>
2023-08-26 16:43:15 +0100

    031 - Testing Promised-based code - Introduction to Microtasks

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -12,12 +12,28 @@ describe('Async Testing Examples', () => {
         }, 1000);
     });
 
-    fit('Asynchronous test example - setTimeout()', fakeAsync(() => {
+    it('Asynchronous test example - setTimeout()', fakeAsync(() => {
         let test = false;
         setTimeout(() => {
             test = true;
         }, 1000);
         flush();
+        expect(test).toBeTruthy();
+    }));
+
+    fit('Asynchronous test example - plain Promise', (() => {
+        let test = false;
+        console.log('Creating promise');
+        setTimeout(() => console.log('setTimeout 1'));
+        setTimeout(() => console.log('setTimeout 2'));
+        Promise.resolve().then(() => {
+            console.log('Resolved promise 1');
+            return Promise.resolve();
+        }).then(() => {
+            console.log('Resolved promise 2');
+            test = true;
+        });
+        console.log('Running assertions');
         expect(test).toBeTruthy();
     }));
 });
